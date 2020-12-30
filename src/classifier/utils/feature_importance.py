895dc8f8c229dc6074554617ab4b7d25136d6dce
Max <47034840+maxscheijen@users.noreply.github.com>
2020-12-30 13:17:14 +0100

    Word highlighting (#5)
    * add word higlighting
    
    * fix lowercasing character if word is start of sentence
    
    * add additional info
    
    * fix model caching

--- src/classifier/utils/feature_importance.py
+++ src/classifier/utils/feature_importance.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 import numpy as np
 
@@ -9,7 +10,7 @@ def fi_trained_model(model):
     return fi
 
 
-def feature_importance_prediction(model, text):
+def feature_importance_prediction(model, text, cuttoff=0.5):
     fi = fi_trained_model(model)
     dense_features = (model.named_steps['text_processors']
                       .transform([text])
@@ -20,4 +21,29 @@ def feature_importance_prediction(model, text):
                                                  ascending=False))
     fi_predict['Sentiment'] = np.where(fi_predict['Belangrijkheid'] > 0,
                                        "Positief", "Negatief")
-    return fi_predict.reset_index(drop=True)
+    fi_predict = fi_predict.loc[(fi_predict["Belangrijkheid"] > cuttoff) |
+                                (fi_predict["Belangrijkheid"] < -cuttoff)]
+    fi_predict['Belangrijkheid'] = fi_predict['Belangrijkheid'].round(2)
+    fi_predict = fi_predict.reset_index(drop=True)
+
+    # Uppercase first character if is start of sentence
+    for word in text.split(" "):
+        if word[0].isupper() and word.lower() in fi["Woord"].values:
+            fi_predict["Woord"] = fi_predict["Woord"].replace(
+                word.lower(), word.title())
+
+    return fi_predict
+
+
+def feature_importance_in_text(fi_df, text):
+
+    replace_dict = pd.Series(
+        np.where(fi_df.Sentiment.str.contains("Positief"), '<b><font color="green">' +
+                 fi_df.Woord + '</font></b>', '<b><font color="red">' + fi_df.Woord + '</font></b>'),
+        index=fi_df.Woord.values).to_dict()
+
+    for word, replace in replace_dict.items():
+        text = re.sub(" " + r"[a-zA-Z]" + word[1:] + " ",
+                            " " + replace + " ", text)
+
+    return text
